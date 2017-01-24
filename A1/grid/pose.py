e672fe3175f409f02736e38fa56dfc82b47b5aef
ZettaVolt <jon.sawin33@gmail.com>
2017-01-24 14:30:12 -0500

    Heuristic 5 working living in pose now

--- A1/grid/pose.py
+++ A1/grid/pose.py
@@ -1,3 +1,4 @@
+from gridcell import GridCell
 
 class Pose:
     """
@@ -11,28 +12,29 @@ class Pose:
         self.gridcell = gridcell
         self.direction = direction
         self.heuristic = heuristic
+        self.__calc_heuristic__()
 
     def __calc_heuristic__(self):
         goal = self.gridcell.get_goal()
         pos = self.gridcell.get_position()
-	    if self.heuristic == 1:
+        if self.heuristic == 1:
 	        self.h_val = 0
-	    elif self.heuristic == 2:
-	        self.h_val = min(abs(pos[0] - goal[0]), abs(pos[1] - goal[1]))
-	    elif self.heuristic == 3:
-	        self.h_val = max(abs(pos[0] - goal[0]), abs(pos[1] - goal[1]))
-	    elif self.heuristic == 4:
-	        self.h_val = abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])
-	    elif self.heuristic == 5:
-            goalcell = GridCell(self.goal, self.goal, 0)
-            direction = self.gridcell.get_direction_to_cell(goalcell)
-            if len(direction) == 1:
+        elif self.heuristic == 2:
+            self.h_val = min(abs(pos[0] - goal[0]), abs(pos[1] - goal[1]))
+        elif self.heuristic == 3:
+            self.h_val = max(abs(pos[0] - goal[0]), abs(pos[1] - goal[1]))
+        elif self.heuristic == 4:
+            self.h_val = abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])
+        elif self.heuristic == 5:
+            goalcell = GridCell(goal, goal, 0)
+            dir_to_goal = self.gridcell.get_direction_to_cell(goalcell)
+            if len(dir_to_goal) == 1:
                 self.h_val = 1
-            elif len(direction) == 2:
+            elif len(dir_to_goal) == 2:
                 self.h_val = 2
-            if direction[0] == 'N':
+            if self.direction in dir_to_goal:
                 self.h_val -= 1
-	    else:
+        else:
 	        # TODO
 	        pass
 
@@ -61,3 +63,18 @@ class Pose:
 
     def get_heuristic(self):
         return self.heuristic
+
+g1 = GridCell([0, 0], [10, 10], 8)
+g2 = GridCell([10, 10], [0, 0], 8)
+g3 = GridCell([5, 5], [8, 5], 8)
+g4 = GridCell([5, 5], [2, 5], 8)
+
+p1 = Pose(g1, 'N', 5)
+p2 = Pose(g2, 'N', 5)
+p3 = Pose(g3, 'N', 5)
+p4 = Pose(g4, 'N', 5)
+
+assert(p1.get_h_val() == 2)
+assert(p2.get_h_val() == 1)
+assert(p3.get_h_val() == 1)
+assert(p4.get_h_val() == 0)
