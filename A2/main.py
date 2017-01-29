02744c5bde2fed31c19d85bb34dd1983265b9d58
Arthur Lockman <hello@rthr.me>
2017-01-29 14:07:20 -0500

    Added help and shuffling

--- A2/main.py
+++ A2/main.py
@@ -1,11 +1,33 @@
 from input_parser import InputParser
 import sys
+from random import shuffle
 
 
 def main():
-    filename = sys.argv[1]
-    run_time = sys.argv[2]
-    print(InputParser.parse_input(filename))
+    if len(sys.argv) < 4:
+        print('Error: incorrect input arguments selected.')
+        print()
+        print('Usage: python3 main.py [optimization type] [filename] ' +
+              '[time limit]')
+        print('       optimization type: hill, annealing, or ga')
+        print('       filename: the input filename to read')
+        print('       time limit: the time limit (in seconds) to limit ' +
+              'the optimization to')
+        exit()
+    algorithm_type = sys.argv[1]
+    filename = sys.argv[2]
+    run_time = sys.argv[3]
+    randomized_input = shuffle(InputParser.parse_input(filename))
+    print(randomized_input)
+    if algorithm_type is 'hill':
+        print('Hill climbing')
+    elif algorithm_type is 'annealing':
+        print('Annealing')
+    elif algorithm_type is 'ga':
+        print('Genetic Algorithm')
+    else:
+        print('Unsupported algorithm.')
+        exit()
 
 
 if __name__ == '__main__':
