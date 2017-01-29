828a68906f05122d1299e89a2bb0c99c0da54af1
tuckerhaydon <tuckerhaydon@gmail.com>
2017-01-29 13:22:32 -0500

    Added bin class

---
+++ A2/bin.py
@@ -0,0 +1,15 @@
+
+class Bin:
+
+    def __init__():
+        pass
+
+    def score():
+        pass
+
+    def append():
+        pass
+
+    @classmethod
+    def swap(bin1, index1, bin2, index2):
+        pass
