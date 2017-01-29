8f4a2f5be680ebb847b09711d5370c1f8eed725b
Arthur Lockman <hello@rthr.me>
2017-01-29 14:21:27 -0500

    Creates the 3 bins.

--- A2/main.py
+++ A2/main.py
@@ -1,7 +1,7 @@
 from input_parser import InputParser
 import sys
 from random import shuffle
-
+from bin import *
 
 def main():
     if len(sys.argv) < 4:
@@ -18,6 +18,10 @@ def main():
     filename = sys.argv[2]
     run_time = sys.argv[3]
     randomized_input = shuffle(InputParser.parse_input(filename))
+    _split = len(randomized_input) / 3
+    bin_1 = Bin1(randomized_input[0:_split])
+    bin_2 = Bin2(randomized_input[_split:2*_split)
+    bin_3 = Bin3(randomized_input[2*_split:len(randomized_input)])
     print(randomized_input)
     if algorithm_type is 'hill':
         print('Hill climbing')
