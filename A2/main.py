2830df33feec87e6d39c8e2835a6e3f59e3859c5
tuckerhaydon <tuckerhaydon@gmail.com>
2017-01-31 13:12:50 -0500

    merge part 2

--- A2/main.py
+++ A2/main.py
@@ -36,7 +36,6 @@ def main():
     elif algorithm_type == 'annealing':
         best_bin_1, best_bin_2, best_bin_3, best_score = run_annealing(input_array, run_time)
     elif algorithm_type == 'ga':
-        print('Genetic Algorithm')
         best_bin_1, best_bin_2, best_bin_3, best_score = run_genetic(input_array, run_time)
     else:
         print('Unsupported algorithm.')
@@ -130,26 +129,14 @@ def run_genetic():
     # Get the size of each bin. The length of input is guarenteed to be divisible by 3
     _split = len(input_array) // 3
 
-    # Initialize the bins
-    best_bin_1 = None
-    best_bin_2 = None
-    best_bin_3 = None
-
     # Split the input array into 3 portions
     bin_1 = Bin1(input_array[0:_split])
     bin_2 = Bin2(input_array[_split:2 * _split])
     bin_3 = Bin3(input_array[2 * _split:len(input_array)])
 
-    _b1, _b2, _b3, _score, run_time = GeneticAlgorithm(bin_1, bin_2, bin_3, run_time).run()
+    GeneticAlgorithm(bin_1, bin_2, bin_3, run_time).run()
 
-    # If the score is better than the current best, log that
-    if _score > best_score:
-        best_score = _score
-        best_bin_1 = _b1
-        best_bin_2 = _b2
-        best_bin_3 = _b3
-
-    return (best_bin_1, best_bin_2, best_bin_3, best_score)
+    return (None, None, None, 0)
 
 def tune_annealing():
     temperature_schedule_functions = [
